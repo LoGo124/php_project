4e0df3cf7ffce96c22f8e3697f2ca872e2bd31d8
LoGo124 <nacholopezgomez2002@gmail.com>
2022-12-02 16:02:02 +0100

    Start

---
+++ Frontend/creaDB.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="estils.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "../Backend/ex1_main.php"?>
+    <title>Incidencies 1</title>
+</head>
+<body>
+    <?php
+        createIncidencies();
+    ?>
+</body>
+</html>
